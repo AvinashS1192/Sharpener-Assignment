9dfa27ea2f97900e9a51af1b70d14fec488f3a28
Avinash Singh <avinashbsingh1192@gmail.com>
2025-09-15 22:50:27 +0530

    added

---
+++ Fs Module/fs mod 2/app.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    fs.readFile("fsmod.txt", (err, data) => {
+      let fdata = data.toString().replaceAll("+", " ");
+
+      res.setHeader("Content-Type", "text/html");
+      console.log(fdata);
+      res.end(`  <h3> ${fdata}</h3>       
+         <form action="/message" method="POST"> 
+        <label>Name: </label>
+        <input type="text" name="username"/>
+        <button type="submit">Add </button>
+        </form>
+        `);
+    });
+  } else if (req.url == "/message") {
+    res.setHeader("Content-Type", "text/html");
+    let data = [];
+    req.on("data", (chunk) => {
+      data.push(chunk);
+    });
+
+    req.on("end", () => {
+      let concatData = Buffer.concat(data).toString().split("=");
+
+      fs.writeFile("fsmod.txt", concatData[1], (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        res.end();
+      });
+      //   console.log(concatData);
+    });
+  }
+});
+const port = 3000;
+
+server.listen(port, () => {
+  console.log("server runnig on http://localhost:3000");
+});
