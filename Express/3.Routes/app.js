10daf241a2e0ee243180fa6cc7a78fc97f952005
Avinash Singh <avinashbsingh1192@gmail.com>
2025-09-15 22:27:21 +0530

    add sharpner assignment

---
+++ Express/3.Routes/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const port = 3000;
+const app = express();
+
+app.get("/orders", (req, res) => {
+  res.send("Here is the list of all orders");
+});
+app.post("/orders", (req, res) => {
+  res.send("A new order has been created.");
+});
+app.get("/users", (req, res) => {
+  res.send("Here is the list of all users.");
+});
+app.post("/users", (req, res) => {
+  res.send("A new user has been added.");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
+});
